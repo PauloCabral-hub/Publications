75085258c51dc3048ac5f058caedb92aa4cfe791
Paulo <paulorcpassos@gmail.com>
2023-08-01 16:42:02 -0300

    adding and correcting functions

---
+++ Passos_etal2023/functions_for_tau_estimation/cut_branch.m
@@ -0,0 +1,114 @@
+% tau_est = cut_branch(s,tau_est, pos, chain, rt)
+%
+% Returns the estimated tree after deciding if a terminal brach should
+% be prunned or not.
+%
+% INPUT:
+%
+% s       = string that induces the terminal branch
+% tau_est = estimated tree
+% pos     = positions in the estimated tree of the strings induced by s
+% chain   = conditioning sequence follwowing tau_real function
+% rt      = conditioned sequence of real numbers
+%
+% OUTPUT:
+%
+% tau_est = the estimated tree after a prunning procedure
+%
+% AUTHOR: Paulo Passos     MODIFIED: 01/08/2023
+
+
+function tau_est = cut_branch(s,tau_est, pos, chain, rt)
+
+
+sind = cell(1,length(pos));
+for a = 1:length(pos)
+sind{1,a} = tau_est{1,pos(1,a)};        
+end
+
+[~, ~, count] = count_contexts(sind, chain);
+
+nh = find(count == 0);
+sit = length(pos)-length(nh);
+
+% case 1: all induced strings occured
+
+if (isempty(nh))&&(length(pos) ~= 1)
+% disp('Case 1')
+ncut = prun_criteria(sind,chain, rt);
+    if ncut ~= 1
+        if brother_suffofacontext(tau_est, tau_est{1,pos(1,1)}, 3)
+            % disp('subcase 3.1')
+            % Cleaning zero counts
+            if find(count == 0)
+            tau_est = clean_zerocounts(tau_est, pos, count);
+            end
+        else
+            % disp('subcase 3.2')
+            tau_est = removing_branch_how(tau_est,pos,s,0);
+        end
+    end
+end
+
+% case 2: any of the induced strings occured
+
+if length(pos) == length(nh)
+% disp('Case 2')
+    tau_est = removing_branch_how(tau_est,pos,s,0);
+end
+
+
+% case 3: a single string occurred
+
+if sit == 1 
+% disp('Case 3')    
+posx =  pos(1,find(count ~= 0)); %#ok<FNDSB>
+    if brother_suffofacontext(tau_est, tau_est{1,posx}, 3)
+        % disp('subcase 3.1')
+        % cleaning zero counts
+        if find(count == 0)
+        tau_est = clean_zerocounts(tau_est, pos, count);
+        end
+    else
+        % disp('subcase 3.2')
+        tau_est = removing_branch_how(tau_est,pos,s,0);
+    end
+end
+
+% case 4 : 2 or more induced strings occurred
+
+if ( sit >= 2 )&&( sit < (length(pos)) )
+% disp('Case 4')
+    % replacing non-occurring contexts by empty strings
+    sind = cell(1,length(pos)); auxpos = [];
+    for a = 1:length(pos)
+        if count(a,1) == 0
+        tau_est{1,pos(1,a)} = [];
+        else
+        auxpos = [auxpos pos(1,a)]; sind{1,a} = tau_est{1,pos(1,a)}; %#ok<AGROW>
+        end
+    end
+    pos = auxpos;
+    % choosing if it should be prunned
+    % option 1
+    jump = 0;
+    for a = 1:length(pos)
+        if brother_suffofacontext(tau_est, tau_est{1,pos(a)}, 3)
+           jump = 1;
+        end
+    end
+    if jump == 0
+    ncut = prun_criteria(sind,chain, rt);
+        if ncut ~= 1
+        tau_est = removing_branch_how(tau_est,pos,s,0); 
+        end        
+    end
+end
+    
+
+% cleaning
+tau_est = removing_branch_how(tau_est,[],[], 1);
+
+end
+
+
---
+++ Passos_etal2023/functions_for_tau_estimation/tauest_real.m
@@ -0,0 +1,82 @@
+% [tau_est] = tauest_real(alphal, rt, chain, show)
+%
+% Returns <tau_est> the estimated tree from a conditioning sequence of sym-
+% bols with alphabet A = {0,1,2} and the conditioned sequence of  real num-
+% bers <rt>. In Passos_etal2023, <chain> corresponds to the goalkeeper  se-
+% quence, while <rt> corresponds to a sequence of response times.
+%
+% INPUT:
+% 
+% alphal = positive integer that corresponds to the length of the alphabet.
+% rt     = row vector containing the sequence of real numbers.
+% chain  = row vector containing the conditioning sequence.
+% show   = if set to 1, presents the graphical representation of the tree
+%
+% OUTPUT:
+%
+% tau_est = cell in which each entry corresponds to a context of the  esti-
+% mated tree
+%
+% AUTHOR: Paulo Roberto Cabral Passos Last MODIFIED: 01/08/2023
+
+
+function [tau_est] = tauest_real(alphal, rt, chain, show)
+
+    % building the alphabet
+    A = zeros(1,alphal);
+    for a = 1:alphal
+        A(1,a) = a-1;
+    end
+    L = floor(log10(length(chain))/log10(3));
+    %
+
+    perm = permwithrep(A,L);
+
+    tau_est = cell(1,length(A)^L);
+
+    for a = 1:size(perm,1)
+    tau_est{1,a} = perm(a,:);
+    end
+
+    for h = 1:L
+        if (L-h) == 0 % visiting root branch.
+            perm = [];
+            branchs = 1;
+        else
+            perm = permwithrep(A,L-h);
+            branchs = size(perm,1);
+        end
+        % finding the leaft of the branch
+        for a = 1:branchs
+           pos = []; 
+           for b = 1:length(tau_est)
+                if (branchs == 1)&&(L-h == 0) % comment: visiting the root branch
+                    if length(tau_est{1,b}) == 1 
+                            pos = [pos b]; %#ok<AGROW> no test if equal
+                    end                    
+                else
+                    if(length(tau_est{1,b}) == (length(perm(a,:))+1))
+                        aux = sum(perm(a,:) == tau_est{1,b}(2:end),2)/length(perm(a,:)); % comment: test if equal
+                        if  aux == 1
+                            pos = [pos b]; %#ok<AGROW>
+                        end
+                    end    
+                end
+           end
+        %  
+        % pruning procedure
+            if ~isempty(pos)
+                if isempty(perm) % root branch
+                    tau_est = cut_branch(perm,tau_est, pos, chain, rt);
+                else % Other branches
+                    tau_est = cut_branch(perm(a,:),tau_est, pos, chain, rt);
+                end
+            end
+        end
+        %
+    end
+
+    if show == 1
+    draw_contexttree(tau_est, A)
+    end
+end
